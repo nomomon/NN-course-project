1c77bbe6b8595c700675946b4205f4e4772ebf65
jwagenbach <55742061+jwagenbach@users.noreply.github.com>
2022-06-12 14:27:16 +0200

    experiment 4: including atom count in input

--- model.py
+++ model.py
@@ -12,24 +12,26 @@ print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('
 print("Num CPUs Available: ", len(tf.config.experimental.list_physical_devices('CPU')))
 
 #get the data
-u = pd.read_csv('./data/processed/bond_types.csv').to_numpy()
+bond_types = pd.read_csv('./data/processed/bond_types.csv').to_numpy()
+no_of_atoms = pd.read_csv('./data/processed/atom_counts.csv').to_numpy()
+u = np.concatenate([bond_types, no_of_atoms], axis=1)
 y = pd.read_csv('./data/processed/energy.csv').to_numpy()
 #split into training and testing
 u_train, _, y_train, _ = train_test_split(u, y, test_size=.2, shuffle=True, random_state=42)
 
 #make the model
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(16, activation = 'relu'))
+model.add(tf.keras.layers.Dense(8, activation = 'relu'))
 model.add(tf.keras.layers.Dense(1))
 
 model.compile(optimizer="Adam", loss="mse", metrics="mae")
 
 #save training logs
-callback = tf.keras.callbacks.CSVLogger('training_logs_Adam_16.csv')
+callback = tf.keras.callbacks.CSVLogger('training_logs_Adam_with_noAtoms.csv')
 
 #save best model
 best_model_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath = 'best_model_Adam_16',
+    filepath = 'best_model_Adam_with_noAtoms',
     monitor='val_loss',
     save_best_only=True,
     save_weights_only=False,
@@ -40,5 +42,5 @@ no_epochs = 300
 
 #run and save the model
 model.fit(u_train, y_train, epochs = no_epochs, validation_split=.2, callbacks=[callback])
-model.save('model_Adam_16')
+model.save('model_Adam_with_noAtoms')
 
--- plot_results.py
+++ plot_results.py
@@ -13,16 +13,18 @@ print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('
 print("Num CPUs Available: ", len(tf.config.experimental.list_physical_devices('CPU')))
 
 #get the data
-u = pd.read_csv('./data/processed/bond_types.csv').to_numpy()
+bond_types = pd.read_csv('./data/processed/bond_types.csv').to_numpy()
+no_of_atoms = pd.read_csv('./data/processed/atom_counts.csv').to_numpy()
+u = np.concatenate([bond_types, no_of_atoms], axis=1)
 y = pd.read_csv('./data/processed/energy.csv').to_numpy()
 #split into training and testing
 _, u_validate, _, y_validate = train_test_split(u, y, test_size=.2, shuffle=True, random_state=42)
 
-model = tf.keras.models.load_model('model_Adam_16')
+model = tf.keras.models.load_model('model_Adam_with_noAtoms')
 
 print(model.evaluate(u_validate, y_validate))
 
-df = pd.read_csv('training_logs_Adam_16.csv')
+df = pd.read_csv('training_logs_Adam_with_noAtoms.csv')
 df.plot(
     x = 'epoch',
     y = ['loss', 'val_loss']
