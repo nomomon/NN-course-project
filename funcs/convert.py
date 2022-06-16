e217ff5c7deb4adcbf348fca38f544650c1a225e
Mansur <funisinfinity26@gmail.com>
2022-06-16 14:34:02 +0200

    cleaning code

--- funcs/convert.py
+++ funcs/convert.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 shortest_bond_length = 80
-longest_bond_length = 187
+longest_bond_length = 200
 
 type_bond_length =  {'BrC': [191], 'BrN': [188], 'BrO': [180], 'BrS': [218], 'CC': [134, 154, 140], 'CCl': [176], 'CF': [141], 'CH': [114], 'CN': [132, 151], 'CO': [124, 143], 'CS': [162, 181], 'ClN': [173], 'ClO': [165], 'ClS': [203], 'FN': [138], 'FO': [130], 'FS': [168], 'HN': [111], 'HO': [103], 'HS': [141], 'NN': [130, 148], 'NO': [122, 140], 'NS': [160, 178], 'OO': [132], 'OS': [170], 'SS': [208]}
+
+def get_bond_name(atom_1, atom_2):
+    return "".join(sorted([atom_1, atom_2]))
 
 def get_bond_type(bond_name, bond_length):
     bond_type = np.argmin([abs(l - bond_length) for l in type_bond_length[bond_name]]) + 1
@@ -29,8 +32,10 @@ def get_bond_type_matrix(distance_matrix, symbols):
     
     for i in range(n_atoms - 1):
         for j in range(i, n_atoms):
-            bond_name = "".join(sorted(symbols[i] + symbols[j]))
+            bond_name = get_bond_name(symbols[i], symbols[j])
             bond_length = distance_matrix[i][j]
+
+            print(bond_name, bond_length)
             
             if(bond_length < shortest_bond_length or 
                 bond_length > longest_bond_length or
@@ -52,7 +57,7 @@ def get_bond_counts(distance_matrix, symbols):
     
     for i in range(n_atoms - 1):
         for j in range(i, n_atoms):
-            bond_name = "".join(sorted(symbols[i] + symbols[j]))
+            bond_name = get_bond_name(symbols[i], symbols[j])
             bond_length = distance_matrix[i][j]
             
             if(bond_length < shortest_bond_length or 
